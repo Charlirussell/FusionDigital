f7df399da00204e4670f293c31d0736b4ba59692
Charli Russell <131972828+Charlirussell@users.noreply.github.com>
2024-04-17 18:16:22 +0200

    Added firebase dependencies for hosting

--- fusiondigital/src/components/UnderConstruction/UnderConstruction.jsx
+++ fusiondigital/src/components/UnderConstruction/UnderConstruction.jsx
@@ -10,7 +10,7 @@ const UnderConstruction = () => {
       <img src={homeImage} alt="fusion-image" />
       <div className='construction-text'>
         <h1>Site en cours de construction</h1>
-        <p>Nous travillions actuellement sur cette site. Veuillez revenir plus tard.</p>
+        <p>Nous travillions actuellement sur ce site. Veuillez revenir plus tard.</p>
       </div>
       </div>
     </div>
