2fc2f600e1df495179d13dd30663159fa673a6b1
Charli Russell <charlirussell2491@gmail.com>
2024-08-08 22:05:49 +0200

    Finished modals on services page. - Added graphics, ecommerce and coaching gifs and png images. - Created modals accordingly. - Edited the scss to align the modals using grid to format them in 3 by three rows in desktop mode and 2 by 2 on mobile devices.

--- fusiondigital/src/components/Services/Services.jsx
+++ fusiondigital/src/components/Services/Services.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { useState } from 'react';
 import ButtonImage1 from '../../assets/images/website-design.gif';
 import ButtonImage2 from '../../assets/images/digital-marketing.gif';
-import ButtonImage3 from '../../assets/images/seo.gif';
+import ButtonImage3 from '../../assets/images/graphics.gif';
+import ButtonImage4 from '../../assets/images/e-commerce.gif';
+import ButtonImage5 from '../../assets/images/seo.gif';
+import ButtonImage6 from '../../assets/images/coaching.gif';
 import StaticImage1 from '../../assets/images/site-web.png';
 import StaticImage2 from '../../assets/images/marketing.png';
-import StaticImage3 from '../../assets/images/seo-ss.png';
+import StaticImage3 from '../../assets/images/graphics.png';
+import StaticImage4 from '../../assets/images/e-commerce.png';
+import StaticImage5 from '../../assets/images/seo-ss.png';
+import StaticImage6 from '../../assets/images/coaching.png';
 
 
 import './Services.scss';
@@ -40,10 +46,31 @@ const Services = () => {
     },
     {
       id: 3,
-      title: 'Optimisation SEO',
-      content: "Chez Fusion Digital, nous sommes spécialisés dans l'optimisation SEO pour améliorer votre visibilité sur les moteurs de recherche. Grâce à des techniques avancées et une connaissance approfondie des algorithmes, nous vous aidons à atteindre les premières positions sur Google et à attirer un trafic qualifié. Notre équipe travaille en étroite collaboration avec vous pour identifier les mots-clés pertinents, optimiser votre contenu et améliorer la structure de votre site. Contactez-nous dès maintenant pour maximiser votre présence en ligne et augmenter votre chiffre d'affaires !",
+      title: 'Graphisme',
+      content: "Chez Fusion Digital, nous excellez dans la Conception Visuelle et Graphisme, en créant des logos percutants, des gifs animés, des vidéos captivantes, et des flyers attrayants pour vos événements. Notre équipe de designers talentueux s'assure que chaque élément visuel reflète parfaitement l'identité de votre marque et capte l'attention de votre audience. Que ce soit pour renforcer votre image de marque ou pour donner vie à vos idées, nous travaillons main dans la main avec vous pour concevoir des visuels sur mesure qui se démarquent. Contactez-nous dès maintenant pour donner une nouvelle dimension à votre communication visuelle et marquer les esprits !",
       image: ButtonImage3,
       staticImage: StaticImage3,
+    },
+    {
+      id: 4,
+      title: 'E-Commerce',
+      content: "Chez Fusion Digital, nous sommes spécialisés dans le développement et l'optimisation de solutions e-commerce adaptées à vos besoins spécifiques. Que vous souhaitiez créer une boutique en ligne avec Shopify ou améliorer votre présence sur d'autres plateformes de commerce électronique, notre équipe experte est là pour vous accompagner à chaque étape. Nous concevons des sites e-commerce attrayants, fonctionnels et performants qui offrent une expérience utilisateur fluide et engageante. Notre approche personnalisée vous aide à atteindre vos objectifs commerciaux en ligne, en optimisant la navigation, le design et les fonctionnalités de votre boutique. Contactez-nous dès maintenant pour transformer votre idée en une boutique en ligne prospère et efficace !",
+      image: ButtonImage4,
+      staticImage: StaticImage4,
+    },
+    {
+      id: 5,
+      title: 'Optimisation SEO',
+      content: "Chez Fusion Digital, nous sommes spécialisés dans l'optimisation SEO pour améliorer votre visibilité sur les moteurs de recherche. Grâce à des techniques avancées et une connaissance approfondie des algorithmes, nous vous aidons à atteindre les premières positions sur Google et à attirer un trafic qualifié. Notre équipe travaille en étroite collaboration avec vous pour identifier les mots-clés pertinents, optimiser votre contenu et améliorer la structure de votre site. Contactez-nous dès maintenant pour maximiser votre présence en ligne et augmenter votre chiffre d'affaires !",
+      image: ButtonImage5,
+      staticImage: StaticImage5,
+    },
+    {
+      id: 6,
+      title: 'Coaching',
+      content: "Vous ne souhaitez pas faire appel à des experts pour gérer votre présence en ligne ? Pas de problème ! Avec notre coaching digital personnalisé, nous vous apprendrons à le faire vous-même. Grâce à des séances de coaching individuelles, nos experts vous guideront pas à pas pour développer vos compétences et maîtriser les outils nécessaires à la gestion efficace de votre stratégie numérique. Contactez-nous dès maintenant pour devenir autonome et optimiser votre présence en ligne grâce à notre accompagnement sur mesure !",
+      image: ButtonImage6,
+      staticImage: StaticImage6,
     },
   ];
 
@@ -67,15 +94,18 @@ const Services = () => {
       ))}
 
       {showModal && (
-        <div className='modal'>
-          <div className='modal-content'>
-            <button className='close-modal-btn' onClick={() => setShowModal(false)}>
-              X
-            </button>
-            <h2>{modalContent.title}</h2>
-            <p>{modalContent.content}</p>
+        <>
+          <div className='modal-overlay' onClick={() => setShowModal(false)}></div>
+          <div className='modal'>
+            <div className='modal-content'>
+              <button className='close-modal-btn' onClick={() => setShowModal(false)}>
+                X
+              </button>
+              <h2>{modalContent.title}</h2>
+              <p>{modalContent.content}</p>
+            </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
