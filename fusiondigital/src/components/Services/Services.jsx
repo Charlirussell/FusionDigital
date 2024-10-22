a6410a76b56004e1a9e2c028399337959f957d51
Charli Russell <charlirussell2491@gmail.com>
2024-10-22 14:36:14 +0200

    Redesigned services card. - Added icons to assets/icons file. - Adjusted the layout of the services card to align the logo and heading side by side, then the paragraph and button-link below. - Changed the class layout, adding a 'service-details' class rather than a seperate class for each card to allow for better styling. - Updated scss for responsivity.

--- fusiondigital/src/components/Services/Services.jsx
+++ fusiondigital/src/components/Services/Services.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import boxLogo from '../../assets/icons/close.png';
+import designLogo from '../../assets/icons/design-icon.png';
+import devLogo from '../../assets/icons/dev-icon.png';
+import seoLogo from '../../assets/icons/seo-icon.png';
+import graphicLogo from '../../assets/icons/graphic-icon.png';
 
 
 import './Services.scss';
@@ -11,27 +14,37 @@ const Services = ({  }) => {
   return (
     <div className='services-container'>
       <div className='services-box'>
-        <div className='services-card-1'>
-          <img src={boxLogo} alt="Design Web" />
-          <h2>Design Web</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <a href="/design"><button>Design Web</button></a>
+        <div className='services-card'>
+          <img src={designLogo} alt="Design Web" />
+          <div className='service-details'>
+            <h2>Design Web</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <a href="/design"><button>Design Web</button></a>
+          </div>
         </div>
-        <div className='services-card-2'>
-          <img src={boxLogo} alt="Dev Web" />
-          <h2>Développement Web</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <a href="/development"><button>Développement Web</button></a>
+        <div className='services-card'>
+          <img src={devLogo} alt="Dev Web" />
+          <div className='service-details'>
+            <h2>Développement Web</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <a href="/development"><button>Développement Web</button></a>
+          </div>
         </div>
-        <div className='services-card-3'><img src={boxLogo} alt="Marketing" />
-          <h2>Marketing Digital</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <a href="/marketing"><button>Marketing Digital</button></a>
+        <div className='services-card'>
+          <img src={seoLogo} alt="Marketing" />
+          <div className='service-details'>
+            <h2>Marketing Digital</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <a href="/marketing"><button>Marketing Digital</button></a>
+          </div>
         </div>
-        <div className='services-card-4'><img src={boxLogo} alt="Graphisme" />
-          <h2>Graphisme</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <a href="/graphics"><button>Graphisme</button></a>
+        <div className='services-card'>
+          <img src={graphicLogo} alt="Graphisme" />
+          <div className='service-details'>
+            <h2>Graphisme</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <a href="/graphics"><button>Graphisme</button></a>
+          </div>
         </div>
       </div>
     </div>
