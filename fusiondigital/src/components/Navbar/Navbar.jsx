d0e03a33bfcac21af87d87e00962909425f5da7b
Charli Russell <charlirussell2491@gmail.com>
2024-10-22 13:51:56 +0200

    Remove dropdown menu logic due to layout and alignment issues
    - Removed all dropdown functionality and related CSS for Services link
    - Dropdown caused alignment issues, and maintaining it would complicate future updates
    - Simplified navigation to avoid potential problems when adding more pages

--- fusiondigital/src/components/Navbar/Navbar.jsx
+++ fusiondigital/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,6 @@ import './Navbar.scss';
 
 const NavBar = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const [showDropdown, setShowDropdown] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu)
@@ -14,14 +13,12 @@ const NavBar = () => {
   const closeMenu = () => {
     if (window.innerWidth > 768) {
       setShowMenu(false);
-      setShowDropdown(false);
     }
   };
 
   const handleLinkClick = () => {
     if (window.innerWidth <= 768) {
-      showMenu(false);
-      setShowDropdown(false);
+      setShowMenu(false);
     }
   };
 
@@ -40,18 +37,10 @@ const NavBar = () => {
         <div className={`burger-icon ${showMenu ? 'close' : 'open'}`}></div>
       </div>
       <div className={`navbar ${showMenu ? 'show-menu' : ''}`}>
-      <NavLink className={({ isActive }) =>isActive ? 'menu menu--active' : 'menu'} to="/" onClick={handleLinkClick}>Accueil</NavLink>
-      {/* Dropdown */}
-      <div className="dropdown" onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
-        <NavLink className={({ isActive }) =>isActive ? 'menu menu--active' : 'menu'} to="/services" onClick={handleLinkClick}>Services<span className="arrow">&#x276F;</span></NavLink>
-        {showDropdown && (
-          <div classname= "dropdown-tab">
-            <NavLink className={({ isActive }) =>isActive ? 'menu menu--active' : 'menu'} to="/design" onClick={handleLinkClick}>Design Web</NavLink>
-          </div>
-        )}
-      </div>
-      <NavLink className={({ isActive }) =>isActive ? 'menu menu--active' : 'menu'} to="/portfolio" onClick={handleLinkClick}>Portfolio</NavLink>
-      <NavLink className={({ isActive }) =>isActive ? 'menu menu--active' : 'menu'} to="/contacter" onClick={handleLinkClick}>Nous Contacter</NavLink>
+        <NavLink className={({ isActive }) =>isActive ? 'menu menu--active' : 'menu'} to="/" onClick={handleLinkClick}>Accueil</NavLink>
+        <NavLink className={({ isActive }) =>isActive ? 'menu menu--active' : 'menu'} to="/services" onClick={handleLinkClick}>Services</NavLink>
+        <NavLink className={({ isActive }) =>isActive ? 'menu menu--active' : 'menu'} to="/portfolio" onClick={handleLinkClick}>Portfolio</NavLink>
+        <NavLink className={({ isActive }) =>isActive ? 'menu menu--active' : 'menu'} to="/contacter" onClick={handleLinkClick}>Nous Contacter</NavLink>
       </div>
     </nav>
   );
