9b2744b1804e6d4636bbc60ea79a062100993610
Charli Russell <charlirussell2491@gmail.com>
2025-01-27 19:13:46 +0100

    Changed the home tagline for something more catchy and modern

--- fusiondigital/src/components/Home/Home.jsx
+++ fusiondigital/src/components/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className='home-content'>
         <img src={homeImage} alt="fusion-image" />
         <div className='home-text'>
-          <h2 className='home-tagline'>Transformez vos idées en réalité numérique – Conception, Développement et Visibilité Web sur mesure</h2>
+          <h2 className='home-tagline'>Là où l’innovation rencontre la créativité</h2>
           <button className='call-to-action'><span>C'est parti !</span></button>
         </div>
       </div>
