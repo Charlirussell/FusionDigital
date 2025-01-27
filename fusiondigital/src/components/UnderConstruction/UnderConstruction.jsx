e585e0698b0d15b06e5e2588b67ded38c263df4d
Charli Russell <charlirussell2491@gmail.com>
2025-01-28 00:31:16 +0100

    Added call and email buttons as well as emojis to the under construction page to make it more visually pleasing and allow people to contact from the consturction page. Also set under construction to true

--- fusiondigital/src/components/UnderConstruction/UnderConstruction.jsx
+++ fusiondigital/src/components/UnderConstruction/UnderConstruction.jsx
@@ -8,8 +8,10 @@ const UnderConstruction = () => {
       <div className='construction-content'>
         <img src={homeImage} alt="fusion-image" />
         <div className='construction-text'>
-          <h1>Site en cours de construction</h1>
-          <p>Nous travaillons actuellement sur ce site. Veuillez revenir plus tard.</p>
+          <h1>🚧 Site en cours de construction 🚧</h1>
+          <p>🔜 Nous travaillons actuellement sur ce site. Veuillez revenir plus tard 🔜</p>
+          <button className="small-button"><a href="mailto:charlifusiondigital@gmail.com">Envoyer un mail 📧</a></button>
+          <button className="small-button"><a href="tel:0638826940">Un coup de fil ? 📱</a></button>
         </div>
       </div>
     </div>
